127e879101e450956d3694ace81c7bff94298413
mcclowes <maxcc@me.com>
2018-12-04 10:51:02 +0000

    regex

--- soundboard.js
+++ soundboard.js
@@ -15,49 +15,49 @@ bot.on('ready', function (evt) {
   isReady = true;
 });
 
-const soundboard = {
-  ah: './sounds/ahhh.mp3',
-  ahh: './sounds/ahhh.mp3',
-  ahhh: './sounds/ahhh.mp3',
-  ahhhh: './sounds/ahhh.mp3',
-  oof: './sounds/oof.mp3',
-  ooof: './sounds/oof.mp3',
-  fart: './sounds/fart.mp3',
-  toot: './sounds/toot.mp3',
-  haha: './sounds/haha.mp3',
-  hahaha: './sounds/haha.mp3',
-  hehe: './sounds/haha.mp3',
-  strong: './sounds/strong.mp3',
-  stable: './sounds/strong.mp3',
+const soundboard = [
+  ['ah', './sounds/ahhh.mp3'],
+  ['oof', './sounds/oof.mp3'],
+  ['ooof', './sounds/oof.mp3'],
+  ['fart', './sounds/fart.mp3'],
+  ['toot', './sounds/toot.mp3'],
+  ['haha', './sounds/haha.mp3'],
+  ['hehe', './sounds/haha.mp3'],
+  ['strong', './sounds/strong.mp3'],
+  ['stable', './sounds/strong.mp3'],
+]
+
+const playSound = async (sound, voiceChannel) => {
+  const connection = await voiceChannel.join();
+  const dispatcher = connection.playFile(sound[1]);
+
+  dispatcher.on('finish', () => {
+    console.log('Finished playing!');
+  });
+  dispatcher.on("end", end => {voiceChannel.leave()});
+
+  return;
 }
 
 // when client receives message
-bot.on("message", async message => {
-  if(isReady) { // check bot is not handling existing command
+bot.on("message", message => {
+  if(isReady === true) { // check bot is not handling existing command
     isReady = false;
-    
-    // parse message and take first command only
-    const command = message.content.toLowerCase();
 
-    if (soundboard[command]) { // only handle "ahhh"
-      const voiceChannel = message.member.voiceChannel;
+    const voiceChannel = message.member.voiceChannel;
+    if (voiceChannel && voiceChannel.joinable) {
+      const command = message.content.toLowerCase();
 
-      if (!voiceChannel) { // if not voice channel, fail
-        isReady = true;
-        return message.reply("User was not in a voice channel")
+      for(let i = 0; i < soundboard.length; i++) {
+        const sound = soundboard[i];
+
+        if (command.match(new RegExp(`${sound[0]}`))) { // only handle "ahhh"
+          playSound(sound, voiceChannel);
+        }
       }
-
-      if (voiceChannel.joinable) {
-        const connection = await voiceChannel.join();
-        const dispatcher = connection.playFile(soundboard[command]);
-
-        dispatcher.on('finish', () => {
-          console.log('Finished playing!');
-        });
-        dispatcher.on("end", end => {voiceChannel.leave()});
-      } else {
-        console.log("VC not joinable")
-      }
+    } else { // if not voice channel, fail
+      isReady = true;
+      return;
     }
 
     isReady = true;
