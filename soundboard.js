ab75c5ff06c51cb74ac8e48cf2866213e8a93c96
mcclowes <maxcc@me.com>
2018-12-04 11:17:39 +0000

    isReady guard logic

--- soundboard.js
+++ soundboard.js
@@ -16,34 +16,30 @@ bot.on('ready', function (evt) {
 });
 
 const soundboard = [
-  ['ah', './sounds/ahhh.mp3'],
-  ['oof', './sounds/oof.mp3'],
-  ['ooof', './sounds/oof.mp3'],
   ['fart', './sounds/fart.mp3'],
   ['toot', './sounds/toot.mp3'],
   ['haha', './sounds/haha.mp3'],
   ['hehe', './sounds/haha.mp3'],
   ['strong', './sounds/strong.mp3'],
   ['stable', './sounds/strong.mp3'],
+  ['ah', './sounds/ahhh.mp3'],
+  ['oof', './sounds/oof.mp3'],
 ]
 
 const playSound = async (sound, voiceChannel) => {
   const connection = await voiceChannel.join();
+
   const dispatcher = connection.playFile(sound[1]);
 
-  dispatcher.on('finish', () => {
-    console.log('Finished playing!');
+  dispatcher.on("end", end => {
+    voiceChannel.leave();
+    return;
   });
-  dispatcher.on("end", end => {voiceChannel.leave()});
-
-  return;
 }
 
 // when client receives message
-bot.on("message", message => {
+bot.on("message", async message => {
   if(isReady === true) { // check bot is not handling existing command
-    isReady = false;
-
     const voiceChannel = message.member.voiceChannel;
     if (voiceChannel && voiceChannel.joinable) {
       const command = message.content.toLowerCase();
@@ -51,8 +47,9 @@ bot.on("message", message => {
       for(let i = 0; i < soundboard.length; i++) {
         const sound = soundboard[i];
 
-        if (command.match(new RegExp(`${sound[0]}`))) { // only handle "ahhh"
-          playSound(sound, voiceChannel);
+        if (isReady === true && command.match(new RegExp(`${sound[0]}`))) { // only handle "ahhh"
+          isReady = false;
+          await playSound(sound, voiceChannel);
         }
       }
     } else { // if not voice channel, fail
